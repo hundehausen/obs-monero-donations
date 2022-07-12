00cb6de786e3ea59c5315d0d03a15e06db89ef6d
Grischa Daum <grischa.daum@sevdesk.de>
2022-07-12 14:11:59 +0200

    working again, no es6 modules

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
-import 'dotenv/config'
-import { connectToWalletRpc } from 'monero-javascript';
-import escape from 'escape-html';
-import qrcode from 'qrcode-generator';
-import express from 'express';
-import bodyparser from 'body-parser';
-const { urlencoded } = bodyparser;
-import { createServer } from 'http2';
-import { Server } from 'socket.io';
-
+require("dotenv").config();
+const monerojs = require("monero-javascript");
+const { connectToWalletRpc } = monerojs;
+const escape = require("escape-html");
+const qrcode = require("qrcode-generator");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const adminApp = express();
 
-app.set('view engine', 'pug');
-adminApp.set('view engine', 'pug');
-
-const server = createServer(app);
-const adminServer = createServer(adminApp);
-const io = new Server(server);
-const io2 = new Server(adminServer);
-
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
+adminApp.set("view engine", "pug");
+const server = require("http").createServer(app);
+const adminServer = require("http").createServer(adminApp);
+const io = require("socket.io")(server);
+const io2 = require("socket.io")(adminServer);
 let name, message, subaddress, amount;
-
-mainFunction();
 
 async function mainFunction() {
   // connect to a monero-wallet-rpc endpoint with authentication
@@ -38,13 +32,13 @@ async function mainFunction() {
   );
   const primaryAddress = await walletRpc.getPrimaryAddress();
   const balance = await walletRpc.getBalance();
-  console.log('Wallet:', process.env.WALLET_NAME);
-  console.log('Primary address:', primaryAddress);
+  console.log("Wallet:", process.env.WALLET_NAME);
+  console.log("Primary address:", primaryAddress);
   console.log(`Balance: ${balance / Math.pow(10, 12)} XMR`);
 
   async function generateNewSubaddress(label) {
     const subaddress = await walletRpc.createSubaddress(0, label);
-    console.log('New Subaddress:', subaddress.state.address);
+    console.log("New Subaddress:", subaddress.state.address);
     return subaddress.state.address;
   }
 
@@ -58,43 +52,42 @@ async function mainFunction() {
     console.log(`Admin Server running on port ${adminServer.address().port}`);
   });
 
-  io.on('connection', (socket) => {
-    console.log('User connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+  io.on("connection", (socket) => {
+    console.log("User connected");
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
   });
 
-  app.get('/', function (req, res) {
+  app.get("/", function (req, res) {
     res.render(process.env.PLATFORM, {
-      title: 'Donate to your favorite Streamer with Monero',
+      title: "Donate to your favorite Streamer with Monero",
       streamerName: process.env.STREAMER_NAME,
       platform: process.env.PLATFORM,
     });
   });
 
-  adminApp.get('/animation', function (req, res) {
-    res.render('animation');
+  adminApp.get("/animation", function (req, res) {
+    res.render("animation");
   });
 
-  app.use(urlencoded({ extended: true }));
   app.post(
-    '/payment',
+    "/payment",
     async function (req, res, next) {
       name = escape(req.body.name);
       message = escape(req.body.message);
 
-      subaddress = await generateNewSubaddress(name + ' - ' + message);
+      subaddress = await generateNewSubaddress(name + " - " + message);
 
       // QR Code generieren
       const typeNumber = 0;
-      const errorCorrectionLevel = 'L';
+      const errorCorrectionLevel = "L";
       const qr = qrcode(typeNumber, errorCorrectionLevel);
       qr.addData(await subaddress);
       qr.make();
       const qrcodeURL = qr.createDataURL(6);
 
-      res.render('payment', {
+      res.render("payment", {
         subaddress: subaddress,
         qrcode: qrcodeURL,
         name: name,
@@ -112,9 +105,9 @@ async function mainFunction() {
             if (payment.state.address == subaddress) {
               amount = payment.state.amount;
               console.log(
-                'Transaction incoming: ' +
+                "Transaction incoming: " +
                   (await amount) / Math.pow(10, 12) +
-                  ' XMR from: ' +
+                  " XMR from: " +
                   subaddress
               );
               clearInterval(checkForPayment);
@@ -128,28 +121,28 @@ async function mainFunction() {
     },
     function (req, res) {
       // Send payment recieved with amount to Client
-      io.emit('payment_recieved', amount / Math.pow(10, 12), name);
+      io.emit("payment_recieved", amount / Math.pow(10, 12), name);
 
       // Generate Animation for OBS
-      io2.emit('new_donation', amount / Math.pow(10, 12), name, message);
+      io2.emit("new_donation", amount / Math.pow(10, 12), name, message);
 
       // Confirmations
       let confirmations_old = 0;
-      let checkConfirmations = setInterval(
+      const checkConfirmations = setInterval(
         async () => {
-          let payments = await walletRpc.getTransfers({
+          const payments = await walletRpc.getTransfers({
             IsIncoming: true,
             address: subaddress,
           });
 
-          for (let payment of payments) {
+          for (const payment of payments) {
             if (payment.state.address == subaddress) {
-              let confirmations_new = await payment.state.tx.state
+              const confirmations_new = await payment.state.tx.state
                 .numConfirmations;
               if (confirmations_new > confirmations_old) {
                 confirmations_old = confirmations_new;
-                console.log('New confirmation:', confirmations_new);
-                io.emit('confirmations', confirmations_new);
+                console.log("New confirmation:", confirmations_new);
+                io.emit("confirmations", confirmations_new);
               }
             }
           }
@@ -160,3 +153,5 @@ async function mainFunction() {
     }
   );
 }
+
+mainFunction();
