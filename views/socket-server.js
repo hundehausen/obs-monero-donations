197ae1b512abda6213f02753f9a7b4dc88e20ba8
Grischa Daum <grischa.daum@sevdesk.de>
2022-07-12 14:47:23 +0200

    remove debugger

--- views/socket-server.js
+++ views/socket-server.js
@@ -1,7 +1,6 @@
 $(function () {
     const socket = io();
     socket.on('new_donation', (amount, name, message) => {
-        debugger;
         $('#donation-canvas').text(name + " donated " + amount + " XMR!");
         $('#donation-message').text(message);
     });
